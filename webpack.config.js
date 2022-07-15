3beb70d97287af0927719a38080c4fd94cd61b1b
GeekMakerSun <geek@13aq.com>
2022-07-15 22:02:20 +0800

    修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 const path = require('path')
 const CopyPlugin = require("copy-webpack-plugin");
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode: 'production',
-  entry: {
-    index: './src/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js',
-    clean: true
-  },
+    mode: 'production',
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
 
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {from: "assets", force: true},
-        {from: "node_modules/bootstrap/dist", to: "bootstrap", force: true},
-        {from: "node_modules/@popperjs/core/dist", to: "/@popperjs", force: true},
-      ],
-      options: {
-        concurrency: 500,
-      },
-    }),
-  ],
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: "assets", force: true},
+                {from: "node_modules/bootstrap/dist", to: "bootstrap", force: true},
+                {from: "node_modules/@popperjs/core/dist", to: "/@popperjs", force: true},
+            ],
+            options: {
+                concurrency: 500,
+            },
+        }),
+        new HtmlWebpackPlugin()
+    ],
 }
