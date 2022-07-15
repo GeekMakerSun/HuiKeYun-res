5288c4b52375f60e1e79927cb1ccc6dc1611144b
GeekMakerSun <geek@13aq.com>
2022-07-16 00:32:58 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Aegis from 'aegis-web-sdk';
 function getComponent() {
 	const element = document.createElement('div');
 	return import('lodash')
@@ -13,6 +14,16 @@ function getComponent() {
 getComponent().then((component) => {
 	document.body.appendChild(component);
 });
+
+
+const aegis = new Aegis({
+	id: 'DvL19tEQ5GJ4np2mOm', // 上报 id
+	uin: 'xxx', // 用户唯一 ID（可选）
+	reportApiSpeed: true, // 接口测速
+	reportAssetSpeed: true, // 静态资源测速
+	spa: true // spa 应用页面跳转的时候开启 pv 计算
+});
+
 
 if (process.env.NODE_ENV !== 'production') {
 	console.log('现在处于开发模式');
